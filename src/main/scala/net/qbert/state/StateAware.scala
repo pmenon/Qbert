2db83065cad01ed8baa60bcc56f888fc2c2901ea
Prashanth Menon <djunforgetable@gmail.com>
2010-10-29 19:11:32 -0400

    Initial state machine work

--- src/main/scala/net/qbert/state/StateAware.scala
+++ src/main/scala/net/qbert/state/StateAware.scala
@@ -1,5 +1,7 @@
 package net.qbert.state
 
+import scala.collection.mutable
+
 trait StateAware {
   val stateManager: StateManager
 }
@@ -12,4 +14,32 @@ trait StateDriven extends StateAware {
   val initialState: State
   val stateManager = new StateManager(initialState)  
 }
+
+trait StateMachine[T] {
+  var currentState: State = _
+
+  type StateTransitionFunction = PartialFunction[Event, State]
+
+  case class State(name: String)
+
+  case class Event(input: T)
+
+  def when(state: State)(f: StateTransitionFunction) = registerTransition(state.name, f)
+
+  private var transitionMap = new mutable.HashMap[String, StateTransitionFunction]
+  def registerTransition(stateName: String, sf: StateTransitionFunction) = {
+    transitionMap.put(stateName, sf)
+  }
+
+  def actOn(event: Event) = {
+    val nextState = transitionMap.get(currentState.name).orElse( Some(handleEvent(_)) ).get.apply(event)
+    setState(nextState)
+  }
+
+  def handleEvent(e: Event): State
+
+  def setState(newState: State) = currentState = newState
+}
+
+
 
